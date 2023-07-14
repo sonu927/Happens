ee05cbdb1cd3b795945b41818f3cc0a646f14f4f
Sonu Raut <sonu16122001@gmail.com>
2023-07-14 11:36:56 +0530

    First commit - initialize express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error on running the server: ${err}`);
+    }
+
+    console.log(`Server running on port : ${port}`);
+})
